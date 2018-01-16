176dd9910ffa0dc4c85354de07beee396095b79d
Sudhansu Barik <sudhansubarik07@gmail.com>
2018-01-17 01:45:48 +0530

    Removed demo JSON URLs from the app and used live link, implemented AsyncTask.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -76,7 +76,6 @@ public class EarthquakeActivity extends AppCompatActivity {
         // Start the AsyncTask to fetch the earthquake data
         EarthquakeAsyncTask task = new EarthquakeAsyncTask();
         task.execute(USGS_REQUEST_URL);
-
     }
 
     /**
